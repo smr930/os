bdaec7460368abfa9f24c84bd84ba46c1026008d
Syed Raza <razasyedm@yahoo.com>
2016-11-07 15:55:01 -0500

    Initial Commit

---
+++ sos/os.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+void siodisk(int jobnum);
+
+void siodrum(int jobnum, int jobsize, int coreaddress, int direction);
+ // Channel commands siodisk and siodrum are made available to you by the simulator.
+ // siodisk has one argument: job number, of type int and passed by value.
+ // siodrum has four arguments, all of type int and passed by value:
+ // first argument is job number;
+ // second argument is job size;
+ // third argument is starting core address;
+ // fourth argument is interpreted as follows:
+ // 1 => move from core (memory) to drum
+ // 0 => move from drum to core (memory)
+
+void ontrace(); // called without arguments
+void offtrace(); // called without arguments
+ // The 2 trace procedures allow you to turn the tracing mechanism on and off.
+ // The default value is off. WARNING: ontrace produces a blow-by-blow description
+ // of each event and results in an extremely large amount of output.
+ // It should be used only as an aid in debugging.
+ // Even with the trace off, performance statistics are
+ // generated at regular intervals and a diagnostic message appears in case of a crash.
+ // In either case, your OS need not print anything.
+
+void startup()
+{
+ // Allows initialization of static system variables declared above.
+ // Called once at start of the simulation.
+
+}
+
+// INTERRUPT HANDLERS
+    // The following 5 functions are the interrupt handlers. The arguments
+    // passed from the environment are detailed with each function below.
+    // See RUNNING A JOB, below, for additional information
+
+void Crint (long &a, long p[])
+{
+ // Indicates the arrival of a new job on the drum.
+ // At call: p [1] = job number
+ // p [2] = priority
+ // p [3] = job size, K bytes
+ // p [4] = max CPU time allowed for job
+ // p [5] = current time
+
+}
+void Dskint (long &a, long p[])
+{
+ // Disk interrupt.
+ // At call: p [5] = current time
+}
+void Drmint (long &a, long p[])
+{
+ // Drum interrupt.
+ // At call: p [5] = current time
+}
+void Tro (long &a, long p[])
+{
+ // Timer-Run-Out.
+ // At call: p [5] = current time
+}
+void Svc (long &a, long p[])
+{
+ // Supervisor call from user program.
+ // At call: p [5] = current time
+ // a = 5 => job has terminated
+ // a = 6 => job requests disk i/o
+ // a = 7 => job wants to be blocked until all its pending
+ // I/O requests are completed
+}
